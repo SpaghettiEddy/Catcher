97683f49eea537109ebe24d3adf898bb695bfcbf
Eduardo Alejandro Vila <35271827+SpaghettiEddy@users.noreply.github.com>
2024-10-31 22:32:42 -0400

    Cleaned up file structure

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
-
     [SerializeField] private int speed = 4;
+
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator; 
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnMovement(InputValue value) {
         movement = value.Get<Vector2>();
+
+        if (movement.x != 0 || movement.y != 0) {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y); 
+
+            animator.SetBool("IsWalking", true);
+        } else {
+            animator.SetBool("IsWalking", false);
+        }
     }
 
     private void FixedUpdate() {
         rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * movement);
-        // rb.velocity = movement * speed;
-        // rb.AddForce(movement * speed);
     }
 }
